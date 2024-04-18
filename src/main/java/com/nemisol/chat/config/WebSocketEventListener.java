4e829795a50e9b2e5eb40891a9b4d0248070d50b
nemisolv <nemisolv@gmail.com>
2024-04-18 14:02:38 +0700

    simple group chat app

---
+++ src/main/java/com/nemisol/chat/config/WebSocketEventListener.java
@@ -0,0 +1,34 @@
+package com.nemisol.chat.config;
+
+import com.nemisol.chat.payload.ChatMessage;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class WebSocketEventListener {
+
+    private final SimpMessageSendingOperations messagingTemplate;
+    @EventListener
+    public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        String username = (String) headerAccessor.getSessionAttributes().get("username");
+        if (username != null) {
+            log.info("User Disconnected : " + username);
+            ChatMessage chatMessage = new ChatMessage();
+            chatMessage.setType(ChatMessage.MessageType.LEAVE);
+            chatMessage.setSender(username);
+            messagingTemplate.convertAndSend("/topic/public", chatMessage);
+
+
+        }
+
+    }
+}
